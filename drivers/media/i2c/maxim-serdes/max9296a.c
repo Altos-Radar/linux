c338850e4ab188b4c5b8e2b0f25e019f7bc94bbe
Michael Wu <michael.wu@altosradar.com>
2026-03-31 21:51:17 -0400

    Support enabling/disabling ERRB on deserializer

--- drivers/media/i2c/maxim-serdes/max9296a.c
+++ drivers/media/i2c/maxim-serdes/max9296a.c
@@ -318,12 +318,14 @@ MAX9296A_FUNC_GROUPS(gpio, "mfp0", "mfp1", "mfp2", "mfp3", "mfp4", "mfp5", "mfp6
 MAX9296A_FUNC_GROUPS(uart1, "mfp5", "mfp6");
 MAX9296A_FUNC_GROUPS(uart2, "mfp0", "mfp1");
 MAX9296A_FUNC_GROUPS(lock, "mfp1");
+MAX9296A_FUNC_GROUPS(errb, "mfp4");
 
 enum max9692a_func {
 	max9296a_func_gpio,
 	max9296a_func_uart1,
 	max9296a_func_uart2,
 	max9296a_func_lock,
+	max9296a_func_errb,
 };
 
 #define MAX9296A_FUNC(name)						\
@@ -335,6 +337,7 @@ static const struct pinfunction max9296a_functions[] = {
 	MAX9296A_FUNC(uart1),
 	MAX9296A_FUNC(uart2),
 	MAX9296A_FUNC(lock),
+	MAX9296A_FUNC(errb),
 };
 
 #define MAX9296A_PINCTRL_X(x) (PIN_CONFIG_END + x)
@@ -653,6 +656,8 @@ static int max9296a_mux_set(struct pinctrl_dev *pctldev, unsigned selector,
 		}
 		if (group == 1) {
 			ret = regmap_update_bits(priv->regmap, 0x005, 0x80, 0x00);
+		} else if (group == 4) {
+			ret = regmap_update_bits(priv->regmap, 0x005, 0x40, 0x00);
 		}
 		break;
 	case max9296a_func_uart1:
@@ -663,6 +668,9 @@ static int max9296a_mux_set(struct pinctrl_dev *pctldev, unsigned selector,
 		break;
 	case max9296a_func_lock:
 		ret = regmap_update_bits(priv->regmap, 0x005, 0x80, 0x80);
+		break;
+	case max9296a_func_errb:
+		ret = regmap_update_bits(priv->regmap, 0x005, 0x40, 0x40);
 		break;
 	}
 	return ret;
